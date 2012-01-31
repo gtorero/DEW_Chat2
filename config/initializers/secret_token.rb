da651fbce8ade85e3ce89effb3cbbbfb1627ccd0
Gino Torero <gtorero@hotmail.com>
2012-01-30 21:28:03 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DewChat2::Application.config.secret_token = 'dfafadd0b7ee111cc48f96f8cd25803a33a437ef8cb6862035a33d26a487cb65f3c1cf7046e4662fe87835e9636f18ecb7060d56e736b1c56dcf7edef7b3c203'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DewChat2::Application.config.session_store :cookie_store, key: '_dew_chat2_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DewChat2::Application.config.session_store :active_record_store
